dc26e2010656a45a54fbd9c8e0d55189119ebf04
Yoonseo Kim <yo26on@soongsil.ac.kr>
2023-04-14 02:20:34 +0900

    전화번호 목록

---
+++ 프로그래머스/전화번호_목록.py
@@ -0,0 +1,7 @@
+# 접두어인 경우가 있으면 false, 아니면 true
+def solution(phone_book):
+    phone_book.sort()
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
