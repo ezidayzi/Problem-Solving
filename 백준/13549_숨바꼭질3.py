7909e6a02adf341dc6e50d3f6900802cfa4ad765
kimyoonseo <yo26on@soongsil.ac.kr>
2023-08-07 10:27:12 +0900

    13549 숨바꼭질3

---
+++ 백준/13549_숨바꼭질3.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+n, m = map(int, input().split())
+visited = [False] * 200002
+
+queue = deque([(n, 0)])
+visited[n] = True
+
+ans = 0
+
+while queue:
+    v, t = queue.popleft()
+    if v < 100001 and not visited[v*2]:
+        queue.append((v*2, t))
+        visited[v*2] = True
+    if v > 0 and not visited[v-1]:
+        queue.append((v-1, t+1))
+        visited[v-1] = True
+    if v < 200001 and not visited[v+1]:
+        queue.append((v+1, t+1))
+        visited[v + 1] = True
+    if v == m:
+        ans = t
+        break
+
+print(ans)
