7744ef9cc13ea0a9281e02478ea85a560977d03a
kimyoonseo <yo26on@soongsil.ac.kr>
2023-09-02 01:47:20 +0900

    14921 용액 합성하기

---
+++ 백준/14921_용액_합성하기.py
@@ -0,0 +1,25 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+liquid = list(map(int, read().split(' ')))
+
+start = 0
+end = n - 1
+
+s = 1000000001 * 2
+a, b = 1000000001, 1000000001
+
+while start < end:
+    c = liquid[start] + liquid[end]
+    if abs(c) < s:
+        s = abs(c)
+        a = liquid[start]
+        b = liquid[end]
+
+    if liquid[start] + liquid[end] < 0:
+        start = start + 1
+    else:
+        end = end - 1
+
+print(a + b)
