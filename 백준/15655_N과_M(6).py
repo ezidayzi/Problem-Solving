8c8359b8c58e9c19f1e4ba064ea7129445dbac67
kimyoonseo <yo26on@soongsil.ac.kr>
2023-09-12 20:47:07 +0900

    15655 N과 M(6)

---
+++ 백준/15655_N과_M(6).py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+s = list(map(int, input().split()))
+s.sort()
+
+a = []
+def dfs():
+    if len(a) == m:
+        print(' '.join(map(str, a)))
+        return
+
+    for i in s:
+        if len(a) == 0:
+            a.append(i)
+            dfs()
+            a.pop()
+        elif i not in a and i > a[-1]:
+            a.append(i)
+            dfs()
+            a.pop()
+
+dfs()
