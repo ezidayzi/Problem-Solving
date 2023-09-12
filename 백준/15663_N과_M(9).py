c259586f3fb52ada68f040d7e5b60faf105db467
kimyoonseo <yo26on@soongsil.ac.kr>
2023-09-12 21:03:52 +0900

    15663 N과 M(9)

---
+++ 백준/15663_N과_M(9).py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+s = list(map(int, input().split()))
+s.sort()
+visited = [False] * n
+
+a = []
+def dfs():
+    if len(a) == m:
+        print(' '.join(map(str, a)))
+        return
+
+    overlap = 0
+    for i in range(len(s)):
+        if not visited[i] and overlap != s[i]:
+            visited[i] = True
+            a.append(s[i])
+            overlap = s[i]
+            dfs()
+            a.pop()
+            visited[i] = False
+
+dfs()
