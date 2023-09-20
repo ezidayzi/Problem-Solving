f36ab5d4b6ed54e297947cb6f1cbe68440520c0b
김윤서 <yo26on@soongil.ac.kr>
2023-09-20 19:31:12 +0900

    5568 카드 놓기

---
+++ 백준/5568_카드_놓기.py
@@ -0,0 +1,21 @@
+n = int(input())
+k = int(input())
+card = []
+for _ in range(n):
+    card.append((input()))
+
+card_list = []
+def dfs(result, n, picked):
+    if n == k:
+        if result not in card_list:
+            card_list.append(result)
+        return
+
+    for i in range(len(card)):
+        if i not in picked:
+            picked.append(i)
+            dfs(result+card[i], n+1, picked)
+            picked.pop()
+
+dfs("", 0, [])
+print(len(card_list))
